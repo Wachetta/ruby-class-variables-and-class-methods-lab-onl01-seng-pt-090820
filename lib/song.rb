342903f32e515bd2a9b2fc1f1a7d1cadb23d851d
Zachary Darneille <zakdarn@gmail.com>
2020-09-25 21:21:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,7 +37,7 @@ class Song
       if @@genre_count.has_key?("rap")
    @@genre_count.collect do |a, b|
      b + 1
-       if @@genre_count.contains?("pop")
+       if @@genre_count.has_key?("pop")
    @@genre_count.collect do |a, b|
      b + 1
         end
