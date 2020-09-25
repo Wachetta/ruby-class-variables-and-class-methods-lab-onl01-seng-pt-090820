ec3a4404911d6a00a79081d511e0570f7f06f3e0
Zachary Darneille <zakdarn@gmail.com>
2020-09-25 21:23:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,18 +8,18 @@ class Song
     @@artist_count = {}
     @@genre_count = {} 
  
-     def initialize(song_name, artist, genre)
-    @name = song_name
-    @artist = artist
-    @genre = genre
-    @@count += 1
-    @@artists << @artist
-    @@genres << @genre
-    @@artist_count.store(@artist, 0)
-    @@genre_count.store(@genre, 0)
+      def initialize(song_name, artist, genre)
+       @name = song_name
+        @artist = artist
+          @genre = genre
+           @@count += 1
+            @@artists << @artist
+             @@genres << @genre
+               @@artist_count.store(@artist, 0)
+                   @@genre_count.store(@genre, 0)
    
-   #binding.pry
-    end
+         #binding.pry
+       end
    
     def self.count
       @@count
@@ -44,9 +44,5 @@ class Song
           end
     end
  
-     
-      end
-    
- end 
- end
+ 
  end
