2f435f4b085d0ab684efc75e940691a10427c8fa
Zachary Darneille <zakdarn@gmail.com>
2020-09-25 21:31:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,6 +40,7 @@ class Song
        if @@genre_count.has_key?("pop")
         @@genre_count.collect do |a, b|
          b + 1
+        puts "#{a} #{b}"
           end
             #binding.pry
           end
